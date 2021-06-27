be59c215d460aae9a8cec053f6807e91d3b77dd3
Rune <t.d.daanen@outlook.com>
2021-06-27 18:51:19 +0200

    test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import I2C_driver
-# from pynput.keyboard import Key, Listener
+from pynput.keyboard import Key, Listener
 mylcd = I2C_driver.lcd()
 
 AUDIO_URL = './assets/audio/'
@@ -13,21 +13,23 @@ task_4 = ['woord 4?', '', 'story-4']
 repeat = True
 keylogger = []
 
-# def listToString(s): 
-#     str1 = "" 
-#     for ele in s: 
-#         str1 += ele  
-#     return str1 
+def listToString(s): 
+    str1 = "" 
+    for ele in s: 
+        str1 += ele  
+    return str1 
 
-# def on_press(key):
-#     key = str(key).replace("'", "")
-#     if key == Key.enter:
-#         keylogger.clear()
-#     elif key == Key.backspace:
-#         if keylogger:
-#             keylogger.pop()
-#     else:
-#         keylogger.append(key)
+def on_press(key):
+    key = str(key).replace("'", "")
+    if key == Key.enter:
+        keylogger.clear()
+    elif key == Key.backspace:
+        if keylogger:
+            keylogger.pop()
+    else:
+        keylogger.append(key)
+    mylcd.lcd_display_string(listToString(keylogger), 2)
+
 
 def PlaySound(sound):
     os.system(f'omxplayer {AUDIO_URL}{sound}.mp3')
@@ -38,8 +40,13 @@ def awnserLoop(tasks, currectAwnser):
         # TODO display lcd tasks question
         mylcd.lcd_clear()
         mylcd.lcd_display_string(tasks[0], 1)
+        listener = Listener(on_press=on_press)
+        listener.start()
         awnser = input(tasks[0])
+        listener.stop()
+
         if (awnser != currectAwnser):
+            mylcd.lcd_display_string('probeer het opnieuw', 1)
             PlaySound('try-again')
         else:
             if tasks[1] == '':
